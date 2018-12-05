15c0d949a4ddae2fd6a2a96470f8e9940e58e8ff
gouthamkantu <gouthamkantu@gmail.com>
2018-12-05 10:10:10 -0500

    First Complition
    It do works, but should check

--- Die.java
+++ Die.java
@@ -1,13 +1,23 @@
 public class Die{
 
-Private int sides;
-Private int value;
+	private int sides;
+	private int value;
 
-public Die(int value){
-	this.value= value;
-	this.sides = 6;
-	}
+	public Die(int value){
+		this.value= value;
+		this.sides = 6;
+		}
 
+	public int getValue(){
+		return value;
+		}
 
+	public void roll(){
+		value =+ (int)(Math.random()+1)*6;
+		}
+
+	public String toString(){
+		return "Number of sides: "+ sides + ", Value: "+ value;
+		}
 
 }
